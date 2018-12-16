a924afcce9fee8cd937fe5160a2f6ec5f213ee06
이상환 <@DESKTOP-4J3RAR0>
2018-12-17 00:24:00 +0900

    delete println ccc

--- ReactiveRESTfulWebServiceV2/src/main/java/hello/Application.java
+++ ReactiveRESTfulWebServiceV2/src/main/java/hello/Application.java
@@ -12,8 +12,7 @@ public class Application {
 		GreetingWebClient gwc = new GreetingWebClient();
 		
 		System.out.println("aaa");
-		System.out.println("bbb");
 		System.out.println(gwc.getResult());
-		System.out.println("ccc");
+		System.out.println("bbb");
 	}
 }
